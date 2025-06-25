30e3d23365229ea13c832cfe5cb9c22e0b1924b8
Saaiil Basnet <basnetssahil@gmail.com>
2025-06-25 18:40:53 +0545

    server setup, supabase setup completed

---
+++ src/database/connection.ts
@@ -0,0 +1,35 @@
+import {Sequelize} from 'sequelize-typescript'
+
+//object instantation
+const CONNECTION_STRING = process.env.DATABASE_URL;
+if(!CONNECTION_STRING){
+
+      throw new Error('DATABASE_URL is not defined in the environment variables.');
+
+}
+  const sequelize = new Sequelize(CONNECTION_STRING,{
+    dialect : "postgres",
+    models : [__dirname + '/models']
+});
+
+// authentication
+sequelize.authenticate()
+.then(()=>{
+    console.log("Successfully authenticated and connected to Supabase.");
+    
+})
+.catch((error)=>{
+    console.log(error);
+    
+})
+
+// migration 
+sequelize.sync({alter: true}).then(()=>{
+    console.log("Migrated to Supabase!");
+    
+}).catch((error)=>{
+    console.log(error);
+    
+})
+
+export default sequelize
