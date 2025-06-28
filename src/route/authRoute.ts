acf10e573e2a0ea5ade27ccb72451058086e4ba4
prajjwol0 <prajjwolpyakurel11@gmail.com>
2025-06-28 13:57:44 +0545

    Bug fixed

--- src/route/authRoute.ts
+++ src/route/authRoute.ts
@@ -1,12 +1,12 @@
+import express from "express";
+import { signup, signin } from '../controller/authController';
+
+const router = express.Router();
+router.post("/signup", signup);
+router.post("/signin", signin);
 
 
-import express from 'express';
-import {signin, signup} from '../controller/authController'
 
 
-const router=express.Router();
 
-router.post("/signin",signin)
-router.post("/signup",signup)
-
-export default router
+export default router;
