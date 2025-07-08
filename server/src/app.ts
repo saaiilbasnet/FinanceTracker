001d767ac283b529b7b41ad54a527d9dcb0202f1
prajjwol0 <prajjwolpyakurel11@gmail.com>
2025-07-08 21:20:07 +0545

    Cors install

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,14 +2,14 @@ import express from 'express'
 import authRoute from './routes/authRoute'
 import incomeRoute from './routes/incomeRoute';
 import expenseRoute from './routes/expenseRoute';
-
+import corse from 'cors'
 const app = express()
 
 console.log("App is being initialized")
-
+app.use(corse());
 app.use(express.json())
 
-app.use("/auth", authRoute);
+app.use("/api", authRoute);
 app.use('/income', incomeRoute)
 app.use('/expense', expenseRoute)
 
