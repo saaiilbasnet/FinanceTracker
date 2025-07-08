d68ab34076b494cb9bf53ef7d5aea0272ecc29e5
Saaiil Basnet <basnetssahil@gmail.com>
2025-07-08 16:23:07 +0545

    fixed, new authcontroller added

--- server/src/routes/authRoute.ts
+++ server/src/routes/authRoute.ts
@@ -1,6 +1,6 @@
 
 import express from 'express'
-import AuthController from '../controllers/authController'
+import AuthController from '../controllers/auth.controller'
 import asyncErrorHandler from '../services/asyncErrorHandler';
 const router = express.Router()
 
